c1a282312e6d93921153b4ec146a81549f4fba68
rac2030 <m@rac.su>
2018-06-07 14:06:53 +0200

    Added chipID to setup mode to read it out in the beginning as its used in multiple places

--- firmware/src/main.ino
+++ firmware/src/main.ino
@@ -1,6 +1,13 @@
+
+// Identifier of the badge, used for communication
+uint64_t chipid;
+
 void setup()
 {
-	
+    Serial.begin(115200);
+    // Get this devices ID
+    chipid=ESP.getEfuseMac();
+
 
 }
 
