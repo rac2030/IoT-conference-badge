55757b0bf5b105e3cf5a69c89f346ec93afd7e5e
Gonzalo Casas <casas@arch.ethz.ch>
2018-06-17 08:37:35 +0200

    EPD working test code Tested with: NINA breakout (proto), Carrier board (proto), EPD 2.13 b/w/r (SKU: 13448, Production)

--- firmware/examples/GxEPD_SPI_TestExample_NINA/GxEPD_SPI_TestExample_NINA.ino
+++ firmware/examples/GxEPD_SPI_TestExample_NINA/GxEPD_SPI_TestExample_NINA.ino
@@ -1,52 +1,6 @@
-/************************************************************************************
-   GxEPD_SPI_TestExample : test example for e-Paper displays from Dalian Good Display Co., Ltd.: www.good-display.com
-
-   based on Demo Example from Good Display, now available on http://www.good-display.com/download_list/downloadcategoryid=34&isMode=false.html
-
-   Author : J-M Zingg
-
-   Version : see library.properties
-
-   Support: limited, provided as example, no claim to be fit for serious use
-
-*/
-
-// Supporting Arduino Forum Topics:
-// Waveshare e-paper displays with SPI: http://forum.arduino.cc/index.php?topic=487007.0
-// Good Dispay ePaper for Arduino : https://forum.arduino.cc/index.php?topic=436411.0
-
-// mapping suggestion from Waveshare SPI e-Paper to Wemos D1 mini
-// BUSY -> D2, RST -> D4, DC -> D3, CS -> D8, CLK -> D5, DIN -> D7, GND -> GND, 3.3V -> 3.3V
-
-// mapping suggestion from Waveshare SPI e-Paper to generic ESP8266
-// BUSY -> GPIO4, RST -> GPIO2, DC -> GPIO0, CS -> GPIO15, CLK -> GPIO14, DIN -> GPIO13, GND -> GND, 3.3V -> 3.3V
-
-// mapping suggestion for ESP32, e.g. LOLIN32, see .../variants/.../pins_arduino.h for your board
-// NOTE: there are variants with different pins for SPI ! CHECK SPI PINS OF YOUR BOARD
-// BUSY -> 4, RST -> 16, DC -> 17, CS -> SS(5), CLK -> SCK(18), DIN -> MOSI(23), GND -> GND, 3.3V -> 3.3V
-
-// new mapping suggestion for STM32F1, e.g. STM32F103C8T6 "BluePill"
-// BUSY -> A1, RST -> A2, DC -> A3, CS-> A4, CLK -> A5, DIN -> A7
-
-// mapping suggestion for AVR, UNO, NANO etc.
-// BUSY -> 7, RST -> 9, DC -> 8, CS-> 10, CLK -> 13, DIN -> 11
-
-// include library, include base class, make path known
+// GxEPD lib and display drivers
 #include <GxEPD.h>
-
-// select the display class to use, only one
-//#include <GxGDEP015OC1/GxGDEP015OC1.cpp>    // 1.54" b/w
-//#include <GxGDEW0154Z04/GxGDEW0154Z04.cpp>  // 1.54" b/w/r
-//#include <GxGDE0213B1/GxGDE0213B1.cpp>      // 2.13" b/w
 #include <GxGDEW0213Z16/GxGDEW0213Z16.cpp>  // 2.13" b/w/r
-//#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
-//#include <GxGDEW029Z10/GxGDEW029Z10.cpp>    // 2.9" b/w/r
-//#include <GxGDEW027C44/GxGDEW027C44.cpp>    // 2.7" b/w/r
-//#include <GxGDEW027W3/GxGDEW027W3.cpp>      // 2.7" b/w
-//#include <GxGDEW042T2/GxGDEW042T2.cpp>      // 4.2" b/w
-//#include <GxGDEW042Z15/GxGDEW042Z15.cpp>    // 4.2" b/w/r
-//#include <GxGDEW075T8/GxGDEW075T8.cpp>      // 7.5" b/w
-
 #include GxEPD_BitmapExamples
 
 // FreeFonts from Adafruit_GFX
@@ -55,104 +9,67 @@
 #include <Fonts/FreeMonoBold18pt7b.h>
 #include <Fonts/FreeMonoBold24pt7b.h>
 
-
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
 
 //NINA
-//static const uint8_t SS    = 5; //GPIO28
+//static const uint8_t SS    = 5;  //GPIO28
 //static const uint8_t MOSI  = 23; //GPIO1
 //static const uint8_t MISO  = 19; // not used for waveshare display
 //static const uint8_t SCK   = 18; // GPIO29
 
-
 // NINA
-static const uint8_t DC = 22; //GPIO20
-static const uint8_t RST = 21; //GPIO8
-static const uint8_t BUSYN = 4; //GPIO24
+static const uint8_t DC = 22;      //GPIO20
+static const uint8_t RST = 21;     //GPIO8
+static const uint8_t BUSYN = 4;    //GPIO24
 
 GxIO_Class io(SPI, SS, DC, RST); 
 GxEPD_Class display(io, RST, BUSYN); 
 
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include "FastLED.h"
+#define DATA_PIN 27                // GPIO18
+#define NUM_LEDS 2
+CRGB leds[NUM_LEDS];
+
 void setup()
 {
   Serial.begin(115200);
+  delay(1000); // let serial console settle
+
   Serial.println();
+
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   Serial.println("setup");
-
   display.init(115200); // enable diagnostic output on Serial
-
   Serial.println("setup done");
 }
 
 void loop()
 {
+  // Turn off twice because the rgb leds on the proto board are giving me attitude
+  ledOff();
+  ledOff();
+
+  Serial.println("Showing bitmap");
   showBitmapExample();
+  Serial.println("Bitmap example done");
   delay(2000);
-#if !defined(__AVR)
+
+  Serial.println("drawing corners");
   drawCornerTest();
   showFont("FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
   showFont("FreeMonoBold12pt7b", &FreeMonoBold12pt7b);
   //showFont("FreeMonoBold18pt7b", &FreeMonoBold18pt7b);
   //showFont("FreeMonoBold24pt7b", &FreeMonoBold24pt7b);
-#else
-  display.drawCornerTest();
-  delay(2000);
-  display.drawPaged(showFontCallback);
-#endif
   delay(10000);
 }
 
-#if defined(_GxGDEP015OC1_H_)
-void showBitmapExample()
-{
-  display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2));
-  delay(5000);
-  display.fillScreen(GxEPD_WHITE);
-  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-  delay(5000);
-  showBoat();
+void ledOff() {
+  leds[0] = CRGB::Black;
+  leds[1] = CRGB::Black;
+  FastLED.show();
 }
-#endif
-
-#if defined(_GxGDEW0154Z04_H_)
-#define HAS_RED_COLOR
-void showBitmapExample()
-{
-#if !defined(__AVR)
-  display.drawPicture(BitmapWaveshare_black, BitmapWaveshare_red, sizeof(BitmapWaveshare_black), sizeof(BitmapWaveshare_red), GxEPD::bm_normal);
-  delay(5000);
-#endif
-  display.drawExamplePicture(BitmapExample1, BitmapExample2, sizeof(BitmapExample1), sizeof(BitmapExample2));
-  delay(5000);
-}
-#endif
-
-#if defined(_GxGDE0213B1_H_)
-void showBitmapExample()
-{
-  display.drawBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.drawBitmap(BitmapExample2, sizeof(BitmapExample2));
-  delay(5000);
-#if !defined(__AVR)
-  display.drawBitmap(first, sizeof(first));
-  delay(5000);
-  display.drawBitmap(second, sizeof(second));
-  delay(5000);
-  display.drawBitmap(third, sizeof(third));
-  delay(5000);
-#endif
-  display.fillScreen(GxEPD_WHITE);
-  display.drawBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-  delay(5000);
-  showBoat();
-}
-#endif
 
 #if defined(_GxGDEW0213Z16_H_)
 #define HAS_RED_COLOR
@@ -175,137 +92,6 @@ void showBitmapExample()
   delay(2000);
   display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
   display.update();
-}
-#endif
-
-#if defined(_GxGDEH029A1_H_)
-void showBitmapExample()
-{
-  display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2));
-  delay(5000);
-  display.fillScreen(GxEPD_WHITE);
-  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-  delay(5000);
-  showBoat();
-}
-#endif
-
-#if defined(_GxGDEW029Z10_H_)
-#define HAS_RED_COLOR
-void showBitmapExample()
-{
-#if defined(__AVR)
-  display.drawExamplePicture(BitmapExample1, BitmapExample2, sizeof(BitmapExample1), sizeof(BitmapExample2));
-  delay(5000);
-#else
-  display.drawPicture(BitmapWaveshare_black, BitmapWaveshare_red, sizeof(BitmapWaveshare_black), sizeof(BitmapWaveshare_red));
-  delay(5000);
-  display.drawExamplePicture(BitmapExample1, BitmapExample2, sizeof(BitmapExample1), sizeof(BitmapExample2));
-  delay(5000);
-  display.drawExamplePicture(BitmapExample3, BitmapExample4, sizeof(BitmapExample3), sizeof(BitmapExample4));
-  delay(5000);
-  display.drawExampleBitmap(BitmapWaveshare_black, sizeof(BitmapWaveshare_black));
-  delay(2000);
-  // example bitmaps for b/w/r are normal on b/w, but inverted on red
-  display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2), GxEPD::bm_invert);
-  delay(2000);
-#endif
-  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-}
-#endif
-
-#if defined(_GxGDEW027C44_H_)
-#define HAS_RED_COLOR
-void showBitmapExample()
-{
-  // draw black and red bitmap
-  display.drawPicture(BitmapExample1, BitmapExample2, sizeof(BitmapExample1), sizeof(BitmapExample2));
-  delay(5000);
-  return;
-  display.drawBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.fillScreen(GxEPD_WHITE);
-  display.drawBitmap(0, 0, BitmapExample1, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-}
-#endif
-
-#if defined(_GxGDEW027W3_H_)
-void showBitmapExample()
-{
-  display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-#if !defined(__AVR)
-  display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2));
-#endif
-  delay(2000);
-  display.drawExampleBitmap(BitmapWaveshare, sizeof(BitmapWaveshare));
-  delay(5000);
-  display.fillScreen(GxEPD_WHITE);
-  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-}
-#endif
-
-#if defined(_GxGDEW042T2_H_) || defined(_GxGDEW042T2_FPU_H_)
-void showBitmapExample()
-{
-#if defined(__AVR)
-  display.drawBitmap(BitmapExample1, sizeof(BitmapExample1));
-#else
-  display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2));
-  delay(5000);
-  display.fillScreen(GxEPD_WHITE);
-  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-#endif
-}
-#endif
-
-#if defined(_GxGDEW042Z15_H_)
-#define HAS_RED_COLOR
-void showBitmapExample()
-{
-#if defined(__AVR)
-  display.drawBitmap(BitmapExample1, sizeof(BitmapExample1));
-#else
-  // draw black and red bitmap
-  display.drawPicture(BitmapExample1, BitmapExample2, sizeof(BitmapExample1), sizeof(BitmapExample2));
-  delay(5000);
-  display.drawPicture(BitmapExample3, BitmapExample4, sizeof(BitmapExample3), sizeof(BitmapExample4));
-  delay(5000);
-  display.drawPicture(BitmapWaveshare_black, BitmapWaveshare_red, sizeof(BitmapWaveshare_black), sizeof(BitmapWaveshare_red));
-  delay(5000);
-  display.drawBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-#endif
-}
-#endif
-
-#if defined(_GxGDEW075T8_H_) || defined(_GxGDEW075Z09_H_)
-void showBitmapExample()
-{
-#if defined(__AVR)
-  //display.drawBitmap(BitmapExample1, sizeof(BitmapExample1));
-#else
-  display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
-  delay(2000);
-  display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2));
-  delay(5000);
-  display.fillScreen(GxEPD_WHITE);
-  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-  display.update();
-#endif
 }
 #endif
 
@@ -369,38 +155,4 @@ void drawCornerTest()
   }
   display.setRotation(rotation); // restore
 }
-
-#if defined(_GxGDEP015OC1_H_) || defined(_GxGDE0213B1_H_) || defined(_GxGDEH029A1_H_)
-#include "IMG_0001.h"
-void showBoat()
-{
-  // thanks to bytecrusher: http://forum.arduino.cc/index.php?topic=487007.msg3367378#msg3367378
-  uint16_t x = (display.width() - 64) / 2;
-  uint16_t y = 5;
-  display.fillScreen(GxEPD_WHITE);
-  display.drawExampleBitmap(gImage_IMG_0001, x, y, 64, 180, GxEPD_BLACK);
-  display.update();
-  delay(500);
-  uint16_t forward = GxEPD::bm_invert | GxEPD::bm_flip_x;
-  uint16_t reverse = GxEPD::bm_invert | GxEPD::bm_flip_x | GxEPD::bm_flip_y;
-  for (; y + 180 + 5 <= display.height(); y += 5)
-  {
-    display.fillScreen(GxEPD_WHITE);
-    display.drawExampleBitmap(gImage_IMG_0001, x, y, 64, 180, GxEPD_BLACK, forward);
-    display.updateWindow(0, 0, display.width(), display.height());
-    delay(500);
-  }
-  delay(1000);
-  for (; y >= 5; y -= 5)
-  {
-    display.fillScreen(GxEPD_WHITE);
-    display.drawExampleBitmap(gImage_IMG_0001, x, y, 64, 180, GxEPD_BLACK, reverse);
-    display.updateWindow(0, 0, display.width(), display.height());
-    delay(1000);
-  }
-  display.update();
-  delay(1000);
-}
-#endif
-
 
