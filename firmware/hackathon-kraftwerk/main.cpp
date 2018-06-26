ee69cd6b4dddc17430721a55b477c973a96df4ef
rac2030 <m@rac.su>
2018-06-26 23:35:16 +0200

    Removed tryout leftover code and added disabling wifi and BT on startup

--- firmware/hackathon-kraftwerk/main.cpp
+++ firmware/hackathon-kraftwerk/main.cpp
@@ -1,16 +1,10 @@
 //Header that declares all shared objects between .cpp files
 #include "taskheader.hpp"
 #include <Arduino.h> //for Serial and delay
+#include <esp_wifi.h>
+#include <esp_bt.h>
 
 Scheduler runner, runnerPriority; 
-
-//Pretend, that the t2 task is a special task,
-//that needs to live in file2 object file.
-void t2Callback() {
-    Serial.print("Only task that starts... :-/ why? ");
-    Serial.println(millis());
-}
-Task t2(100, TASK_ONCE, &t2Callback, &runner, true);
 
 // Tasks
 // TODO: Should work iof they are in their own file as it works in the simple example...
@@ -21,11 +15,15 @@ Task tUpdateDisplay(&handleUpdateDisplay, &runner);
 Task tSplashView(100, TASK_ONCE, &handleSplashView, &runnerPriority);
 
 void setup () {
+    // Disable networking parts to save energy until we really need it
+    esp_wifi_stop();
+    esp_bt_controller_disable();
+
     Serial.begin(115200);
     // Initiate the display object
     display.init(115200); // enable diagnostic output on Serial
   
-    Serial.println("MakeZurich Badge Firmware v0.0.1");
+    Serial.println("MakeZurich Badge Firmware v0.0.2");
 
     delay(1000);
     
