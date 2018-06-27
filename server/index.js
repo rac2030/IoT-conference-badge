e26d548e4eeff07cf7d3b5beb7a7aa52e012e6a4
Gonzalo Casas <casas@arch.ethz.ch>
2018-06-27 11:12:21 +0200

    Disable TCP interface unless explicitely enabled

--- server/index.js
+++ server/index.js
@@ -4,9 +4,8 @@ const fmt = require('./serialization')
 const srv = require('./services')
 const log = require('morgan')
 
-const TCP_DELIMITER = '\r'
 const handleRequest = function (request) {
-  let requestType = request['meta']['type'].trim()
+  let requestType = (request['meta']['type'] || '').trim()
   let service = srv[requestType]
   if (!service) {
     throw new Error(`Unknown request type: ${requestType}`)
@@ -14,32 +13,36 @@ const handleRequest = function (request) {
   return service(request)
 }
 
-// Configure TCP interface
-net.createServer(function (socket) {
-  let completeData = ''
-  const name = `${socket.remoteAddress}:${socket.remotePort}`
-  socket.name = name
-  console.log(`Client connected: ${name}`)
+if (process.env.TCP_ENABLE === 'yes') {
+  const TCP_DELIMITER = '\r'
 
-  socket.on('close', () => console.log(`Closed client: ${name}`))
-  socket.on('disconnect', () => console.log(`Disconnected client: ${name}`))
-  socket.on('data', function (data) {
-    try {
-      completeData += data.toString()
-      if (completeData.indexOf(TCP_DELIMITER) < 0) {
-        return
+  // Configure TCP interface
+  net.createServer(function (socket) {
+    let completeData = ''
+    const name = `${socket.remoteAddress}:${socket.remotePort}`
+    socket.name = name
+    console.log(`Client connected: ${name}`)
+
+    socket.on('close', () => console.log(`Closed client: ${name}`))
+    socket.on('disconnect', () => console.log(`Disconnected client: ${name}`))
+    socket.on('data', function (data) {
+      try {
+        completeData += data.toString()
+        if (completeData.indexOf(TCP_DELIMITER) < 0) {
+          return
+        }
+
+        console.log(`Received from client: ${name}, raw data: ${completeData}`)
+        const request = fmt.deserialize(name, completeData)
+        const response = handleRequest(request)
+        socket.write(fmt.serialize(response) + TCP_DELIMITER)
+      } catch (error) {
+        console.error(`Failed to parse message from client: ${name}, raw data: ${completeData}`)
+        console.error(error)
       }
-
-      console.log(`Received from client: ${name}, raw data: ${completeData}`)
-      const request = fmt.deserialize(name, completeData)
-      const response = handleRequest(request)
-      socket.write(fmt.serialize(response) + TCP_DELIMITER)
-    } catch (error) {
-      console.error(`Failed to parse message from client: ${name}, raw data: ${completeData}`)
-      console.error(error)
-    }
-  })
-}).listen(3000).on('listening', () => console.log('Started TCP Server on 3000'))
+    })
+  }).listen(3000).on('listening', () => console.log('Started TCP Server on 3000'))
+}
 
 // Configure HTTP interface
 const app = express()
