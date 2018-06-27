e580695296fa654e3c6a006ab99ce3150192653b
rac2030 <m@rac.su>
2018-06-27 18:00:53 +0200

    Added root CA for SSL communication

--- firmware/hackathon-kraftwerk/taskFetchRegistration.cpp
+++ firmware/hackathon-kraftwerk/taskFetchRegistration.cpp
@@ -16,12 +16,42 @@ HTTPClient http;
 
 void WiFiEvent(WiFiEvent_t event, WiFiEventInfo_t info);
 
+const char* root_ca= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \
+"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+"DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \
+"SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \
+"GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \
+"AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \
+"q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \
+"SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \
+"Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \
+"a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \
+"/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \
+"AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \
+"CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \
+"bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \
+"c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \
+"VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \
+"ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \
+"MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \
+"Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \
+"AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \
+"uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \
+"wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \
+"X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \
+"PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \
+"KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \
+"-----END CERTIFICATE-----\n";
+
 // Handlers
 void handleFetchRegistration()
 {
     // This handler should only be called with a timeout set, otherwise it might run indefinitely
     Serial.println("TODO: fetch registration from server over Kraftwerk wifi");
     esp_wifi_start();
+    delay(3000);
     Serial.println("Connecting to WiFi network: " + String(ssid));
     WiFi.onEvent(WiFiEvent);
     WiFi.begin(ssid, password);
@@ -47,7 +77,10 @@ void handleFetchRegistration()
     String lastName = "";
 
     // Fetch details from server, parse them and update properties if name is returned
-    http.begin(String(GETNAME_ENDPOINT) + efuseMac); //Specify the URL
+    String target = String(GETNAME_ENDPOINT) + efuseMac;
+    Serial.print("Hitting ");
+    Serial.print(target);
+    http.begin(target, root_ca); //Specify the URL
     int httpCode = http.GET();                       //Make the request
 
     if (httpCode > 0)
@@ -55,7 +88,9 @@ void handleFetchRegistration()
         String payload = http.getString();
         Serial.print("HTTP response code: ");
         Serial.println(httpCode);
+        Serial.println("----- Payload start -----");
         Serial.println(payload);
+        Serial.println("----- Payload end -----");
         
         // Parse the response
         const size_t bufferSize = 2*JSON_OBJECT_SIZE(2) + 140;
@@ -82,7 +117,8 @@ void handleFetchRegistration()
 
     else
     {
-        Serial.println("Error on HTTP request");
+        Serial.print("Error on HTTP request: HTTP response code is ");
+        Serial.println(httpCode);
     }
 
     http.end(); //Free the resources
