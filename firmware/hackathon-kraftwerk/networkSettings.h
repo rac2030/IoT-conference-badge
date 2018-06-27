8f31d6bc72d7a4403df38afe70db692998e03ffb
rac2030 <m@rac.su>
2018-06-28 01:30:49 +0200

    fixed sensor data push request to actually work

--- firmware/hackathon-kraftwerk/networkSettings.h
+++ firmware/hackathon-kraftwerk/networkSettings.h
@@ -1,8 +1,8 @@
 #ifndef _networkSettings_H_
     #define _networkSettings_H_
 
-    #define SERVER_BASEPATH "https://golgafrincham-mekeyasfsg.now.sh"
-    #define GETNAME_ENDPOINT "/?type=get-badge-name&id="
+    #define SERVER_BASEPATH "https://golgafrincham-mekeyasfsg.now.sh/"
+    #define GETNAME_ENDPOINT "?type=get-badge-name&id="
 
     // Wifi settings at Kraftwerk
     extern const char * WIFI_SSID;
